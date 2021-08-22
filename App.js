a7857dc65628c19084844aa5c4007c2549e511ec
Tausif Shekh <63940752+TausifM@users.noreply.github.com>
2021-08-22 14:12:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import Hero from './Components/Hero';
+import Navbar from './Components/Navbar';
+import GlobalStyle from './globalStyle';
+
+function App() {
+  return (
+    <div >
+      <GlobalStyle />
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
